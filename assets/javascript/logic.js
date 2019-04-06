9344f4457ac7833bfb750b3b596b6eac34f61b50
javierordz <javier.ordz@gmail.com>
2019-04-06 08:39:46 -0500

    added pdf

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,3 +1,14 @@
-$(document).on("click", "#pfBtn", function (event) {
+var page = ["splash", "portfolio"];
+var state = 0;
+
+$(document).on("click", "#switch-view", function (event) {
     event.preventDefault();
+
+    $(`#${page[state]}`).addClass("hidden-element");
+    state ^= 1;
+
+    if (state) {$("#switchBtn").html("<i class='fas fa-arrow-left'></i><a class='linkText' id='switch-view'>ABOUT</a><i class='fas fa-arrow-left'></i>");
+    } else {$("#switchBtn").html("<i class='fas fa-arrow-right'></i><a class='linkText' id='switch-view'>PORTFOLIO</a><i class='fas fa-arrow-right'></i>");}
+
+    $(`#${page[state]}`).removeClass("hidden-element");
 })
