b0077aedb535fae23866bf25686d1b043de922da
javierordz <javier.ordz@gmail.com>
2019-06-15 07:27:18 -0500

    rewrite of portfolio

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,30 +1,38 @@
-var page = ["splash", "portfolio"];
-var state = 0;
-
-$(document).on("click", ".switchBtn", function (event) {
-    event.preventDefault();
-    $(`#${page[state]}`).addClass("hidden-element");
-    state ^= 1;
-
-    if (state) {
-        $(".switchBtn").html("<i class='fas fa-arrow-left'></i><a class='linkText' class='switch-view'>ABOUT</a><i class='fas fa-arrow-left'></i>");
-    } else {
-        $(".switchBtn").html("<i class='fas fa-arrow-right'></i><a class='linkText' class='switch-view'>PORTFOLIO</a><i class='fas fa-arrow-right'></i>");
-    }
-    $(`#${page[state]}`).removeClass("hidden-element");
+$("#profile-mini").on("click", function () {
+    $("#profile-mini").hide("slow", "swing");
+    $("#portfolio-mini").hide("slow", "swing");
+    $("#bone").animate({ left: "+=50%", opacity: "0" }, "slow", "swing");
+    $("#name").animate({ left: "+=50px" }, "slow", "swing");
+    $("#center-block").animate({ right: "0", width: "50px" }, "slow", "swing");
+    $("#profile").show().animate({ left: "0", opacity: "1" }, "slow", "swing");
+    $("#return-right").show().animate({ opacity: "1"}, "slow", "swing");
 })
 
-$(document).on("click", ".switchBtn-alt", function (event) {
-    event.preventDefault();
-    $(`#${page[state]}`).addClass("hidden-element");
-    state ^= 1;
+$("#portfolio-mini").on("click", function () {
+    $("#profile-mini").hide("slow", "swing");
+    $("#portfolio-mini").hide("slow", "swing");
+    $("#bone").animate({ left: "-=50%", opacity: "0" }, "slow", "swing");
+    $("#center-block").animate({ width: "50px" }, "slow", "swing");
+    $("#portfolio").css("display", "inline-block").animate({ right: "0", opacity: "1" }, "slow", "swing");
+    $("#return-left").show().animate({ opacity: "1"}, "slow", "swing");
+})
 
-    if (state) {
-        $(".switchBtn-alt").html("<i class='fas fa-arrow-left'>&nbsp;&nbsp;</i><a>ABOUT</a>&nbsp;&nbsp;<i class='fas fa-arrow-left'></i>");
-    } else {
-        $(".switchBtn-alt").html("<i class='fas fa-arrow-right'>&nbsp;&nbsp;</i><a>PORTFOLIO</a>&nbsp;&nbsp;<i class='fas fa-arrow-right'></i>");
-    }
-    $(`#${page[state]}`).removeClass("hidden-element");
+$(".return-home").on("click", function () {
+    $("#profile-mini").show("slow", "swing");
+    $("#portfolio-mini").show("slow", "swing");
+    $("#bone").animate({ left: "50%", opacity: "1" }, "slow", "swing");
+    $("#name").animate({ left: "0px" }, "slow", "swing");
+    $("#center-block").animate({ left: "0", width: "50%" }, "slow", "swing");
+    $("#profile").animate({ left: "0", opacity: "0" }, "slow", "swing");
+    $("#portfolio").animate({ right: "0", opacity: "0" }, "slow", "swing")
+    $("#return-left").hide();
+    $("#return-right").hide();
+    setTimeout(function(){
+        $("#center-block").removeAttr("style");
+        $("#portfolio").hide();
+        $("#profile").hide();
+    }, 1000);
+    
 })
 
 // OPENING ANIMATION //
